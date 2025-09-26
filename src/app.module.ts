b0f55b35088d9187895c92967e2539a17f083479
ephadeev <itrex1991@hotmail.com>
2025-09-26 09:46:29 +0300

    connected Users, Animals and Shelters modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { SheltersModule } from './shelters/shelters.module';
+import { AnimalsModule } from './animals/animals.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(`mongodb://localhost:27017/animal-shelters`),
+    UsersModule,
+    SheltersModule,
+    AnimalsModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
