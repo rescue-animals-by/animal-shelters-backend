202bbd4db53f5e074393636f31195e9efadb5f63
ephadeev <itrex1991@hotmail.com>
2025-09-26 09:05:24 +0300

    added swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('animal shelters API')
+    .setDescription('animal shelters API description')
+    .setVersion('1.0')
+    .addTag('animal-shelters')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    useGlobalPrefix: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
